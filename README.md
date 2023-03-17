f24a26810be90d09b156775b9226eed7d4a728b7
Akram <Akram_Man13@hotmail.com>
2023-03-17 12:24:59 +1100

    Updated Schema SQL inputs and readme

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # Good Catch
-A social platform for fishing hobbyists to share their experiences and meet one another.
+A social platform for fishing hobbyists to create a profile and share their fishing  experiences online.
+
+# URL
+This app has been deployed on Render and be accessed at using this link: https://goodcatch.onrender.com/ 
+
+# Development
+At the start of development, a considerable amount of time was spent preparing wireframe drawings of the views that would be displayed to a user during their time on GoodCatch.
+These wireframes were later referred to during the preparation of the views as a convenient way to draft an initial view's HTML and CSS. 
+These wireframes can be viewed at https://whimsical.com/project-2-wireframe-9F9oXCBuvAZuWCWbav25eS
+
+After preparation of wireframes, a preliminary flowchart was prepared to help guide the initial steps of the app's development. This was used initially to help focus attention on the immediate next step in a sequential manner. After approximately ~50% of the app's CRUD features were developed, this tool was discontinued as the remaining tasks could more easily be listed and tracked within the coding files. 
+
+The development was started with the setup of a local repo, a GitHub repo and a Render host service, followed by the installation of the modules that would be required for use of the app. The main technologies used were Postgres and Express. For a list of the modules used, please refer to the package.json file.
+
+
+# Unsolved problems
+Currently the styling of the home page does is not able to display more catches than 12, which is what the number of cells that the existing grid has been setup to handle.
+
+Additionally, the responsiveness of the website is not great. Some of the pages' elements will begin clashing with each other if the page is being viewed on a small screen or a mobile device. Being an app intended for ease of use and quick submissions in the moment, this is a priority and will be addressed in the near future.
+
+# Future additions
+Due to resource constraints, not all the intended features could be implemented as of now. 
+These include:
+- Features to allow users to interact with each other and each others' posts/catches (eg. Catch likes and comment).
+- A search function allowing users to view catches based on select keywords
+- A sort function on the home-page catches 
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -24,19 +24,19 @@ CREATE TABLE users (
 
 
 -- Sample records in Catch to get things started
-INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (1, 'Big catch yo!', 'Went to a local form and caught me some of these - good dinner inbound!', 'https://images.theconversation.com/files/281656/original/file-20190627-76726-dg5e89.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop', 'VIC', 'Dande Farm', '2023-03-12');
+INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (12, 'Big catch yo!', 'Went to a local form and caught me some of these - good dinner inbound!', 'https://images.theconversation.com/files/281656/original/file-20190627-76726-dg5e89.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop', 'VIC', 'Dande Farm', '2023-03-12');
 
-INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (1, 'Hope you like anchovies', 'not sure why I said that, I just really like anchovies, and I think these are it? Please lmk if i''m off guys', 'https://www.science.org/do/10.1126/science.acz9961/abs/_20220105_on_smallerfish.jpg', 'VIC', 'Gippsland', '2023-01-24');
+INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (12, 'Hope you like anchovies', 'not sure why I said that, I just really like anchovies, and I think these are it? Please lmk if i''m off guys', 'https://www.science.org/do/10.1126/science.acz9961/abs/_20220105_on_smallerfish.jpg', 'VIC', 'Gippsland', '2023-01-24');
 
-INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (1, 'Sardines for days', 'Caught a bunch of Sardines @ lakes entrance, hope I didn''t breach any fishing restrictions..', 'https://www.goodfood.com.au/content/dam/images/h/0/z/c/5/x/image.related.socialLead.620x349.h0yx3z.png/1524796963137.jpg', 'VIC', 'Lakes Entrance', '2023-01-24');
+INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (12, 'Sardines for days', 'Caught a bunch of Sardines @ lakes entrance, hope I didn''t breach any fishing restrictions..', 'https://grist.org/wp-content/uploads/2012/05/sardines_andrea_nguyen.jpg', 'VIC', 'Lakes Entrance', '2023-01-24');
 
-INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (2, 'Enough to feed a family', '...of one (not a very big family either). Keen to try again another day still', 'https://i.redd.it/1s58xw6xm2c31.jpg', 'VIC', 'Mornington Pier', '2022-01-05');
+INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (13, 'Enough to feed a family', '...of one (not a very big family either). Keen to try again another day still', 'https://i.redd.it/1s58xw6xm2c31.jpg', 'VIC', 'Mornington Pier', '2022-01-05');
 
-INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (3, 'Full to the bream', 'Yes, that is a bream fish and no I''m not ashamed of that joke. Cooked this last night after me and the kids got home, made for a good meal.', 'https://ondeckby.dinga.com.au/wp-content/uploads/2018/12/BREAM-FISHING-101-FEATURE-1.jpg', 'VIC', 'Gippsland Lake', '2022-04-12');
+INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (14, 'Full to the bream', 'Yes, that is a bream fish and no I''m not ashamed of that joke. Cooked this last night after me and the kids got home, made for a good meal.', 'https://ondeckby.dinga.com.au/wp-content/uploads/2018/12/BREAM-FISHING-101-FEATURE-1.jpg', 'VIC', 'Gippsland Lake', '2022-04-12');
 
-INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (1, 'Catch & Run', 'Caught this fella but wasn''t sure what specie it was so i released him back. Does anyone know what this is?', 'https://www.nps.gov/subjects/fishing/images/Webp_4.jpg?maxwidth=1300&autorotate=false', 'VIC', 'Lakes Entrance', '2023-01-24');
+INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (12, 'Catch & Run', 'Caught this fella but wasn''t sure what specie it was so i released him back. Does anyone know what this is?', 'https://www.nps.gov/subjects/fishing/images/Webp_4.jpg?maxwidth=1300&autorotate=false', 'VIC', 'Lakes Entrance', '2023-01-24');
 
-INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (4, 'Big Bertha', 'I needed all two arms to reel this bad boy in', 'https://www.wildadventuresjm.com.au/wp-content/uploads/2020/08/20200803_163955-e1597018330212.jpg', 'VIC', 'Lakes Exit', '2021-07-13');
+INSERT INTO catches (user_id, caption, experience, image_url, catch_state, catch_location, catch_date) VALUES (15, 'Big Bertha', 'I needed all two arms to reel this bad boy in', 'https://www.wildadventuresjm.com.au/wp-content/uploads/2020/08/20200803_163955-e1597018330212.jpg', 'VIC', 'Lakes Exit', '2021-07-13');
 
 
 -- Sample records in Users to keep things moving :-)
@@ -44,9 +44,11 @@ INSERT INTO users (username, email, password_digest, user_img_url, user_about) V
 
 INSERT INTO users (username, email, password_digest, user_img_url, user_about) VALUES ('iLikeFish', 'test1@ga.co', 'pudding', 'https://static.vecteezy.com/system/resources/previews/005/176/777/non_2x/user-avatar-line-style-free-vector.jpg', 'I''m just here to keep this site running, and not in the devOps sense.');
 
-INSERT INTO users (username, email, password_digest, user_img_url, user_about) VALUES ('I LOVE fish', 'test2@ga.co', 'pudding', 'https://static.vecteezy.com/system/resources/previews/005/176/777/non_2x/user-avatar-line-style-free-vector.jpg', 'I''m just here to keep this site running, and not in the devOps sense.');
+INSERT INTO users (username, email, password_digest, user_img_url, user_about) VALUES ('ILOVEfish', 'test2@ga.co', 'pudding', 'https://static.vecteezy.com/system/resources/previews/005/176/777/non_2x/user-avatar-line-style-free-vector.jpg', 'I''m just here to keep this site running, and not in the devOps sense.');
 
 INSERT INTO users (username, email, password_digest, user_img_url, user_about) VALUES ('HowMuchIsTheFish', 'test4@ga.co', 'pudding', 'https://static.vecteezy.com/system/resources/previews/005/176/777/non_2x/user-avatar-line-style-free-vector.jpg', 'I''m just here to keep this site running, and not in the devOps sense.');
+
+INSERT INTO users (username, email, password_digest, user_img_url, user_about) VALUES ('HowMuchIsTheFish', 'test5@ga.co', 'pudding', 'https://static.vecteezy.com/system/resources/previews/005/176/777/non_2x/user-avatar-line-style-free-vector.jpg', 'I''m just here to keep this site running, and not in the devOps sense.');
 
 -- Digesting the existing user's passwords
 UPDATE users SET password_digest='$2b$10$8i.u1kVDpQ/IEfFY9SkjfOPZTsZcVQE1ov4CkI5CyciDVcEEN69bu' WHERE password_digest='pudding';
